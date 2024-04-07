90cfd3c6271008f19ff7a31624f81887abe66db5
David7370 <159639764+David7370@users.noreply.github.com>
2024-04-07 22:19:52 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -20,36 +20,6 @@ async function fetchWeather() {
     const cloudiness = currentWeather.cloud;
     const hourlyForecast = data.forecast.forecastday[0].hour;
 
-    let backgroundImage;
-    switch (weatherCondition) {
-      case 'sunny':
-        backgroundImage = 'url(https://images.pexels.com/photos/912364/pexels-photo-912364.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)';
-        break;
-      case 'cloudy':
-        backgroundImage = 'url(https://images.pexels.com/photos/158163/clouds-cloudporn-weather-lookup-158163.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)';
-        break;
-      case 'rainy':
-        backgroundImage = 'url(https://images.pexels.com/photos/832521/pexels-photo-832521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)';
-        break;
-      case 'partly cloudy':
-        backgroundImage = 'url(https://images.pexels.com/photos/4691043/pexels-photo-4691043.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)';
-        break;
-      case 'overcast':
-        backgroundImage = 'url(https://images.pexels.com/photos/3742711/pexels-photo-3742711.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)';
-        break;
-      case 'clear':
-        backgroundImage = 'url(https://images.pexels.com/photos/281260/pexels-photo-281260.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)';
-        break;
-      case 'snow':
-        backgroundImage = 'url(https://images.pexels.com/photos/773953/pexels-photo-773953.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)';
-        break;
-      case 'Light snow showers':
-        backgroundImage = 'url(https://images.pexels.com/photos/3593865/pexels-photo-3593865.jpeg)';
-        break;
-    }
-
-    weatherInfo.style.backgroundImage = backgroundImage;
-
     let weatherData = `
       <p>Location: ${data.location.name}, ${data.location.country}</p>
       <p>Current Temperature: ${currentWeather.temp_c}°C</p>
